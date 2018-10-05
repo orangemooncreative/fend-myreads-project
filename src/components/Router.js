94cbd8ca1a002e6a7f3006a0e1700c613809770d
Davida Pitts <Davida@orangemooncreative.com>
2018-10-05 12:06:08 +0800

    Move search page to own component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from '../App';
+import SearchPage from './pages/SearchPage';
 import NotFound from './pages/NotFound';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
+      <Route exact path="/search" component={SearchPage} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
